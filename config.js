e712a16b16fb66636ac45571acecb95fd8cdf6f9
Optitrack Optimove <optimove.develop.optitrack@gmail.com>
2017-07-18 21:02:45 +0300

    Order Of Predefined Events is higher

--- config.js
+++ config.js
@@ -29,30 +29,86 @@ var optimoveTenantConfiguration = {
         popupCallback: null
     },
     events: {
-        addToCart: {
-            id: 9,
+        set_user_id_event: {
+            id: 1001,
             supportedOnOptitrack: true,
             supportedOnRealTime: true,
             parameters: {
-                productName: {
-                    required: true,
-                    name: "action_name",
-                    id: 2,
+                originalVisitorId: {
+                    optional: false,
+                    name: "originalVisitorId",
+                    id: 1,
                     type: "String",
                     optiTrackDimensionId: 8
                 },
-                productId: {
-                    required: false,
-                    name: "action_value",
-                    id: 4,
-                    type: "Number",
+                userId: {
+                    optional: false,
+                    name: "userId",
+                    id: 2,
+                    type: "String",
                     optiTrackDimensionId: 9
                 },
-                price: {
-                    required: true,
-                    name: "action_price",
-                    id: 5,
-                    type: "Number",
+                updatedVisitorId: {
+                    optional: false,
+                    name: "updatedVisitorId",
+                    id: 3,
+                    type: "String",
+                    optiTrackDimensionId: 10
+                }
+            }
+        },
+        Set_email_event: {
+            id: 1002,
+            supportedOnOptitrack: true,
+            supportedOnRealTime: true,
+            parameters: {
+                email: {
+                    optional: false,
+                    name: "email",
+                    id: 1,
+                    type: "String",
+                    optiTrackDimensionId: 8
+                }
+            }
+        },
+         page_category_event: {
+            id: 1003,
+            supportedOnOptitrack: true,
+            supportedOnRealTime: false,
+            parameters: {
+                category: {
+                    optional: false,
+                    name: "category",
+                    id: 1,
+                    type: "String",
+                    optiTrackDimensionId: 8
+                }
+            }
+        },
+        stitch_event: {
+            id: 1004,
+            supportedOnOptitrack: true,
+            supportedOnRealTime: true,
+            parameters: {
+                sourcePublicCustomerId: {
+                    optional: false,
+                    name: "sourcePublicCustomerId",
+                    id: 1,
+                    type: "String",
+                    optiTrackDimensionId: 8
+                },
+                sourceVisitorId: {
+                    optional: false,
+                    name: "sourceVisitorId",
+                    id: 2,
+                    type: "String",
+                    optiTrackDimensionId: 9
+                },
+                targetVsitorId: {
+                    optional: false,
+                    name: "targetVsitorId",
+                    id: 3,
+                    type: "String",
                     optiTrackDimensionId: 10
                 }
             }
@@ -174,92 +230,6 @@ var optimoveTenantConfiguration = {
                     optiTrackDimensionId: 10
                 }
             }
-        },
-
-        
-        set_user_id_event: {
-            id: 1001,
-            supportedOnOptitrack: true,
-            supportedOnRealTime: true,
-            parameters: {
-                originalVisitorId: {
-                    optional: false,
-                    name: "originalVisitorId",
-                    id: 1,
-                    type: "String",
-                    optiTrackDimensionId: 8
-                },
-                userId: {
-                    optional: false,
-                    name: "userId",
-                    id: 2,
-                    type: "String",
-                    optiTrackDimensionId: 9
-                },
-                updatedVisitorId: {
-                    optional: false,
-                    name: "updatedVisitorId",
-                    id: 3,
-                    type: "String",
-                    optiTrackDimensionId: 10
-                }
-            }
-        },
-        Set_email_event: {
-            id: 1002,
-            supportedOnOptitrack: true,
-            supportedOnRealTime: true,
-            parameters: {
-                email: {
-                    optional: false,
-                    name: "email",
-                    id: 1,
-                    type: "String",
-                    optiTrackDimensionId: 8
-                }
-            }
-        },
-         page_category_event: {
-            id: 1003,
-            supportedOnOptitrack: true,
-            supportedOnRealTime: false,
-            parameters: {
-                category: {
-                    optional: false,
-                    name: "category",
-                    id: 1,
-                    type: "String",
-                    optiTrackDimensionId: 8
-                }
-            }
-        },
-        stitch_event: {
-            id: 1004,
-            supportedOnOptitrack: true,
-            supportedOnRealTime: true,
-            parameters: {
-                sourcePublicCustomerId: {
-                    optional: false,
-                    name: "sourcePublicCustomerId",
-                    id: 1,
-                    type: "String",
-                    optiTrackDimensionId: 8
-                },
-                sourceVisitorId: {
-                    optional: false,
-                    name: "sourceVisitorId",
-                    id: 2,
-                    type: "String",
-                    optiTrackDimensionId: 9
-                },
-                targetVsitorId: {
-                    optional: false,
-                    name: "targetVsitorId",
-                    id: 3,
-                    type: "String",
-                    optiTrackDimensionId: 10
-                }
-            }
-        },
+        }            
     }
 }
