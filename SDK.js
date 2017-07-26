20f1267ea72884a8a98be531cb2d6411d7a4123a
MOBIUS\gil_g <gil_g@optimove.com>
2017-07-26 12:39:50 +0300

    changes on real time module xhr call

--- SDK.js
+++ SDK.js
@@ -163,9 +163,9 @@ var optimoveSDK = function(){
                                                                                                  _configuration.realtimeMetaData.realtimeGateway + event : 
                                                                                                  _configuration.realtimeMetaData.realtimeGateway + '/' + event;
             xmlhttp.open("POST", url, true);
-            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded ; charset=utf-8");
+            xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
             xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
+                if (xmlhttp.readyState == XMLHttpRequest.DONE && xmlhttp.status == 200) {
                     var responseData = JSON.parse(this.responseText);
                     if (_configuration.realtimeMetaData.options.popupCallBack) {
                         _configuration.realtimeMetaData.options.popupCallBack(response);
